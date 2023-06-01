f2b18478cdc1192b21ca87020307cf7ee4148a1d
TechStylist <aditipsharma1@gmail.com>
2023-06-01 23:42:15 +0530

    visit

---
+++ script.js
@@ -0,0 +1,29 @@
+// document.querySelector("#")
+
+gsap.from("#cenim",{
+    ease:Expo.easeInOut,
+    right:"-100%",
+    stagger:.3,
+    duration:1,
+    opacity:"100px"
+  })
+gsap.from("#rtop",{
+    ease:Expo.easeInOut,
+    right:"-100%",
+    stagger:.3,
+    duration:1,
+    opacity:"100px"
+  })
+gsap.from("#line",{
+    ease:Expo.easeInOut,
+    left:"-1600px",
+    stagger:.3,
+    duration:1,
+    opacity:1
+  })
+  
+  $('.enlarge').textillate({ in: { effect: 'fadeInUp ' } });
+  $('.enlarge1').textillate({ in: { effect: 'fadeInUp ' } });
+  $('.enlarge2').textillate({ in: { effect: 'fadeInUp'} });
+  $('#one').textillate({ in: { effect: 'fadeInUp ' } });
+  $('#two').textillate({ in: { effect: 'fadeInUp ' } });
